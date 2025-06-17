4adf0e5fab635c62aba6c04ab32fa570128163c8
VishnuVY2017 <https://vishnuvy2017@github.com>
2025-06-16 23:39:11 -0600

    initial project structure setup

---
+++ src/test/java/com/charter/customer/CustomerRewardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charter.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRewardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
